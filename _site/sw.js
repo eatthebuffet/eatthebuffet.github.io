bdb455f8eec00e45d8f386935f154495e24c5b95
eatthebuffet <126613098+eatthebuffet@users.noreply.github.com>
2023-05-07 07:40:38 -0400

    Add files via upload

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230507.111618';
+const cacheName = 'chirpy-20230507.113051';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
